4cfa59b31abd003da3ae1ac309805af716098600
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-07-08 23:32:01 +0200

    Update rejestracja.php

--- rejestracja.php
+++ rejestracja.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rejestracja</title>
     <link rel="stylesheet" href="styl.css">
 </head>
 <body>
@@ -18,13 +18,14 @@
             ?>
         </nav>
         <section>
+            
             <form action="rejskrypt.php" method="GET">
                 login: <input type="text" name="login" id="">
                 <br>
                 hasło: <input type="text" name="haslo" id="">
-
                 <input type="submit" value="WYSLIJ">
             </form>
+            
         </section>
     </main>
 </body>
